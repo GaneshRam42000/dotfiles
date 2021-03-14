c9dea8ed8049a432d6bd0c068c3d3c95c857573c
GaneshRam42000 <ganeshram42000@gmail.com>
2021-03-14 12:44:32 +0530

    with i3-gaps and polybar

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # installation
-packages='sway swaylock kitty waybar rofi neofetch htop mako feh ranger swayidle gammastep azote pavucontrol libappindicator-gtk3 mpv slurp light'
+packages='sway swaylock kitty waybar rofi neofetch htop mako feh ranger swayidle gammastep azote pavucontrol libappindicator-gtk3 mpv slurp light i3-gaps polybar'
 echo "*******************INSTALLING UPDATES*****************"
 dnf -y update
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf copr enable fuhrmann/i3-gaps
 echo "*************INSTALLING REQUIRED PACKAGES*************"
 dnf isntall -y $packages
 dnf remove alacritty dmenu
--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ mv ~/dotfiles/.config/waybar ~/.config/
 mv ~/dotfiles/.config/kitty ~/.config/
 mv ~/dotfiles/.config/mako ~/.config/
 mv ~/dotfiles/.config/swaylock ~/.config/
+mv ~/dotfiles/.config/i3 ~/.config/
+mv ~/dotfiles/.config/polybar ~/.config/
 #copying fonts
 mv ~/dotfiles/.local/share/fonts/ ~/.local/share/
 echo "SETUP COMPLETE NOW YOU CAN LOG IN WITH SWAY"
